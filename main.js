5b4d2cb1e46057964a59d45fa73fdf7bcb8f52f4
TxMarcoRx <58686646+TxMarcoRx@users.noreply.github.com>
2020-03-10 16:31:02 +0100

    First Proto

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-const canvas = document.getElementById("game-space");
+
+/*********************************** html imports **************************************/
+
+const canvas = document.getElementById("game-space");   //canvas setting
 const context = canvas.getContext("2d");
 
-const startBut = document.getElementById("start");
+const startBut = document.getElementById("start");    //buttons to start and stop
 const stopBut = document.getElementById("stop");
+
+/*********************************** classes ***************************************/
 
 class Asteroid {
 
@@ -33,33 +38,56 @@ class Asteroid {
     }
 }
 
-class BigBang {
-
-    swarm = [];
-    spawnClock = 2000;    //time between two asteroids generation
+class BigBang {                 //the object that mind all the asteroids generation
+    swarm = [];                 //array of asteroids
+    spawnClock = 2000;          //time between two asteroids generation
     asteroidTravelSpeed = 100;
     timeFromLastAsteroid = 0;
 
-    act(delta) {
+    /**
+     * Method that create a new asteroid and ad it to the swarm, then move all the asteroids downward
+     * @param delta the diff of time between two renders
+     */
+    explosion(delta) {
         this.timeFromLastAsteroid += delta;
+        /** if the time from the last render is bigger than the current frequency */
         if (this.timeFromLastAsteroid >= this.spawnClock) {
+            /** random size between 5 and 30 */
             const size = 5 + Math.random() * 25;
-            this.swarm.push(new Asteroid(5 + Math.random() * (canvas.width - 10), -20, this.asteroidTravelSpeed, size));
+
+            /** adding a new random position asteroid to the swarm (leaving at least 5px from borders) */
+            this.swarm.push(new Asteroid(size + 5 + (Math.random() * (canvas.width - size - 5), -20, this.asteroidTravelSpeed, size)));
             this.timeFromLastAsteroid -= this.spawnClock;
         }
+        /** moving all the asteroids forward */
         for (const asteroid of this.swarm) {
             asteroid.act(delta);
         }
     }
 
+    /** drawing all to the canvas */
     render() {
         for (const asteroid of this.swarm) {
             asteroid.render();
         }
     }
+
+    /**
+     * To eliminate all the asteroids passed
+     */
+    blackHole() {
+        for (let asteroid in this.swarm) {
+            if (this.swarm[asteroid].pos.y >= canvas.height + this.swarm[asteroid].size + 1)
+                this.swarm.splice(asteroid, 1);
+        }
+        //console.log(this.swarm.length);   debugging test shows that the total number is constant
+    }
 }
 
 class StarShip {
+
+    bullets = [];
+    reloadSpeed = 150;
 
     render() {
         context.beginPath();
@@ -67,16 +95,58 @@ class StarShip {
         context.fillStyle = "#ffffff";
         context.fill();
         context.closePath();
+        for(let bullet of this.bullets){
+            bullet.render();
+        }
+    }
+
+    shoot() {
+        this.bullets.push(new bullet(mousePos.x, mousePos.y, this.reloadSpeed, 1));
+        for(let bullet of this.bullets){
+            bullet.act();
+        }
     }
 }
+
+class bullet {
+
+    speed = null;
+    size = null;
+
+    pos = {
+        x: null, y: null
+    };
+
+    constructor(x, y, speed, size) {
+        this.pos.x = x;
+        this.pos.y = y;
+        this.size = size;
+        this.speed = speed;
+    }
+
+    render() {
+        context.beginPath();
+        context.rect(this.pos.x, this.pos.y, 2, 5);
+        context.fillStyle = "#ffffff";
+        context.fill();
+        context.closePath();
+    }
+
+    act(delta) {
+        this.pos.y -= this.speed * delta / 1000;
+    }
+}
+
+/************************************ global variables **************************************/
 
 let mousePos = null;
 let starship = null;
 let asteroidGenerator = null;
-let gameStatus = "ready";   //ready, stop , inGame
+let gameStatus = "ready";   //ready, stop, inGame, pause
 let score = 0;
-
 let lastRender = Date.now();
+
+/********************************** rendering loop ***************************************/
 
 function renderField() {
 
@@ -88,17 +158,21 @@ function renderField() {
     let delta = now - lastRender;
     lastRender = now;
 
-    console.log(1000 / delta);
-    asteroidGenerator.act(delta);
+    //console.log(1000 / delta); fps
+    asteroidGenerator.explosion(delta);
+    asteroidGenerator.blackHole();
     context.clearRect(0, 0, canvas.width, canvas.height);
     starship.render();
     asteroidGenerator.render();
     setTimeout(renderField, 1000 / 120);
 }
-
 setTimeout(renderField, 1000 / 120);
 
+/************************************************* interface interactions ********************************************************/
 
+/**
+ * making the mouse input for the canvas
+ */
 canvas.addEventListener("mousemove", (ev) => {
         mousePos = {
             x: ev.clientX, y: ev.clientY
@@ -106,6 +180,18 @@ canvas.addEventListener("mousemove", (ev) => {
     }
 );
 
+/**
+ * making the shooting system
+ */
+canvas.addEventListener("click", (ev) => {
+        starship.shoot();
+        //console.log("click");
+    }
+);
+
+/**
+ * start button listener
+ */
 startBut.addEventListener("click", (ev) => {
         if (gameStatus === "ready") {
             starship = new StarShip();
@@ -115,6 +201,9 @@ startBut.addEventListener("click", (ev) => {
     }
 );
 
+/**
+ * stop button listener
+ */
 stopBut.addEventListener("click", (ev) => {
         if (gameStatus === "inGame") {
             gameStatus = "stop";
@@ -123,4 +212,3 @@ stopBut.addEventListener("click", (ev) => {
         }
     }
 );
-
